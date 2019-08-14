cc0c9c2e69321061e11800db868f2b2fd432193e
ElenaPavlova <hell618lena@gmail.com>
2019-08-14 18:42:19 +0500

    Add Entity

--- src/main/java/com/company/app/ServletInitializer.java
+++ src/main/java/com/company/app/ServletInitializer.java
@@ -3,6 +3,7 @@ package com.company.app;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
--- src/main/java/com/company/app/service/ScheduledTask.java
+++ src/main/java/com/company/app/service/ScheduledTask.java
@@ -3,13 +3,14 @@ package com.company.app.service;
 import com.company.app.beans.MyBean;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Slf4j
+@EnableScheduling
 @Service
-@Component
 public class ScheduledTask {
     private MyBean myBean;
 
