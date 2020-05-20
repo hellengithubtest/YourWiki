b6fa6c49d3d34eaa0ec81b28cc3c12fa7300c3f5
Elena Pavlova <epavlova@lanit.ru>
2020-05-20 22:49:30 +0500

    front-052022: Add menu items

--- front/src/app/main-page/main-page.component.ts
+++ front/src/app/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MenuItem} from 'primeng';
 
 @Component({
   selector: 'app-main-page',
@@ -6,10 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent implements OnInit {
+  mainPageMenu: MenuItem[] = [
+    {
+      label: 'ГЛАВНАЯ',
+      id: 'main-page',
+      command: (event) => this.goToMainPage(),
+      icon: ''
+    },
+    {
+      label: 'РАСПИСАНИЕ',
+      id: 'schedule',
+      command: (event) => this.goToSchedule(),
+      icon: ''
+    },
+    {
+      label: 'НАШИ ДОСТИЖЕНИЯ',
+      id: 'achievements',
+      command: (event) => this.goToAchievements(),
+      icon: ''
+    },
+    {
+      label: 'КОНТАКТЫ',
+      id: 'contacts',
+      command: (event) => this.goToContacts(),
+      icon: ''
+    }
 
+  ]
   constructor() { }
 
   ngOnInit() {
   }
+
+
+  // change to route
+  private goToMainPage() {}
+
+  private goToSchedule() {}
+
+  private goToAchievements() {}
+
+  private goToContacts() {}
+
 
 }
